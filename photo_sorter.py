09005a4075dce1ae5c98f1011be0bcacf5eb1217
XzoRit <xzorit@hotmail.com>
2018-01-04 11:04:09 +0100

    focus function names on creation of path names

--- photo_sorter.py
+++ photo_sorter.py
@@ -20,17 +20,20 @@ def creation_date_from_photo(file_name):
     exif_data = extract_exif_data(exif_dict)
     return extract_creation_date(exif_data)
 
-def str_from_time(t):
-    return time.strftime("%Y_%m_%d_%H_%M_%S", t)
+def path_str_from_time(t):
+    return time.strftime("%Y/%m/%Y_%m_%d_%H_%M_%S", t)
 
-def file_name_from_photo_creation_date(photo_file):
-    return str_from_time(creation_date_from_photo(str(photo_file))) + photo_file.suffix
+def path_from_photo_creation_date(photo_file):
+    return Path(
+        path_str_from_time(
+            creation_date_from_photo(str(photo_file)))
+        + photo_file.suffix)
 
 class TestPhotoSorter(unittest.TestCase):
 
-    def test_photo_creation_date(self):
-        photo_file_name = file_name_from_photo_creation_date(Path("test/photo.jpg"))
-        self.assertEqual(photo_file_name, "2015_03_01_14_08_43.jpg")
+    def test_path_from_photo_creation_date(self):
+        file_path = path_from_photo_creation_date(Path("test/photo.jpg"))
+        self.assertEqual(file_path, Path("2015/03/2015_03_01_14_08_43.jpg"))
 
 if __name__ == '__main__':
     unittest.main()
