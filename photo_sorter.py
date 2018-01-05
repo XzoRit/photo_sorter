8e55f13d024925d1a694ab3f4a30166081ae8793
XzoRit <xzorit@hotmail.com>
2018-01-05 11:58:52 +0100

    destination paths shall be in same folder as source paths

--- photo_sorter.py
+++ photo_sorter.py
@@ -21,12 +21,13 @@ def creation_date_from_photo(file_name):
     exif_data = extract_exif_data(exif_dict)
     return extract_creation_date(exif_data)
 
-def path_str_from_time(t):
+def path_str_from_date(t):
     return Path(time.strftime("%Y/%m/%Y_%m_%d_%H_%M_%S", t))
 
 def path_from_photo_creation_date(photo_file):
-    return path_str_from_time(
-        creation_date_from_photo(str(photo_file))).with_suffix(photo_file.suffix)
+    creation_date = creation_date_from_photo(str(photo_file))
+    path_with_creation_date = path_str_from_date(creation_date).with_suffix(photo_file.suffix)
+    return Path(os.path.join(photo_file.parent, path_with_creation_date))
 
 def src_dest_path_from_photo_creation_date(photo_file):
     return {photo_file: path_from_photo_creation_date(photo_file)}
@@ -73,12 +74,12 @@ class TestPhotoSorter(unittest.TestCase):
 
     def test_iterate_over_photo_files(self):
         folder = "test"
-        expected = {  Path("test/photo_0.jpg"): Path("2015/03/2015_03_01_14_08_43_00.jpg")
-                    , Path("test/photo_1.jpg"): Path("2015/03/2015_03_01_14_08_43_01.jpg")
-                    , Path("test/photo_2.jpg"): Path("2015/03/2015_03_01_14_08_43_02.jpg")
-                    , Path("test/photo_3.jpg"): Path("2015/03/2015_03_01_14_08_43_03.jpg")
-                    , Path("test/photo_4.jpg"): Path("2015/03/2015_03_01_14_08_43_04.jpg")
-                    , Path("test/photo_5.jpg"): Path("2015/03/2015_03_01_14_08_43_05.jpg")
+        expected = {  Path("test/photo_0.jpg"): Path("test/2015/03/2015_03_01_14_08_43_00.jpg")
+                    , Path("test/photo_1.jpg"): Path("test/2015/03/2015_03_01_14_08_43_01.jpg")
+                    , Path("test/photo_2.jpg"): Path("test/2015/03/2015_03_01_14_08_43_02.jpg")
+                    , Path("test/photo_3.jpg"): Path("test/2015/03/2015_03_01_14_08_43_03.jpg")
+                    , Path("test/photo_4.jpg"): Path("test/2015/03/2015_03_01_14_08_43_04.jpg")
+                    , Path("test/photo_5.jpg"): Path("test/2015/03/2015_03_01_14_08_43_05.jpg")
         }
         actual = iterate_over_photo_files(folder)
         self.assertDictEqual(actual, expected)
