5fab24f93952f2f848056bf747fe478509f9080d
XzoRit <xzorit@hotmail.com>
2018-01-20 11:35:36 +0100

    put photo file path creation function into own class

--- photo_sorter.py
+++ photo_sorter.py
@@ -4,62 +4,56 @@ import time
 
 from pathlib import Path
 
-def bytes_to_str(bytes):
-    return "".join(map(chr, bytes))
+class PhotoSorter:
+    def __init__(self, photo_folder, dest_folder):
+        self._photo_folder = photo_folder
+        self._dest_folder = dest_folder
 
-def extract_exif_data(exif_dict):
-    return exif_dict["Exif"]
+    def bytes_to_str(self, bytes):
+        return "".join(map(chr, bytes))
 
-def extract_creation_date(exif_data):
-    date_time_original_str = bytes_to_str(exif_data[36867])
-    return time.strptime(date_time_original_str, "%Y:%m:%d %H:%M:%S")
+    def extract_exif_data(self, exif_dict):
+        return exif_dict["Exif"]
 
-def creation_date_from_photo(file_name):
-    exif_dict = piexif.load(file_name)
-    exif_data = extract_exif_data(exif_dict)
-    return extract_creation_date(exif_data)
+    def extract_creation_date(self, exif_data):
+        date_time_original_str = self.bytes_to_str(exif_data[36867])
+        return time.strptime(date_time_original_str, "%Y:%m:%d %H:%M:%S")
 
-def path_str_from_date(t):
-    return Path(time.strftime("%Y/%m/%Y_%m_%d_%H_%M_%S", t))
+    def creation_date_from_photo(self, file_name):
+        exif_dict = piexif.load(file_name)
+        exif_data = self.extract_exif_data(exif_dict)
+        return self.extract_creation_date(exif_data)
 
-def path_from_photo_creation_date(photo_file, dest_folder):
-    creation_date = creation_date_from_photo(str(photo_file))
-    path_with_creation_date = path_str_from_date(creation_date).with_suffix(photo_file.suffix)
-    return Path(os.path.join(dest_folder, path_with_creation_date))
+    def path_str_from_date(self, t):
+        return Path(time.strftime("%Y/%m/%Y_%m_%d_%H_%M_%S", t))
 
-def src_dest_path_from_photo_creation_date(photo_file, dest_folder):
-    return {photo_file: path_from_photo_creation_date(photo_file, dest_folder)}
+    def path_from_photo_creation_date(self, photo_file, dest_folder):
+        creation_date = self.creation_date_from_photo(str(photo_file))
+        path_with_creation_date = self.path_str_from_date(creation_date).with_suffix(photo_file.suffix)
+        return Path(os.path.join(dest_folder, path_with_creation_date))
 
-def make_reversed_multidict(a_dict):
-    reversed_multidict = dict()
-    for key, value in a_dict.items():
-        reversed_multidict.setdefault(value, list()).append(key)
-    return reversed_multidict
+    def src_dest_path_from_photo_creation_date(self, photo_file, dest_folder):
+        return {photo_file: self.path_from_photo_creation_date(photo_file, dest_folder)}
 
-def append_counter_to_path(path, counter):
-    new_file_name = path.stem + "_" + "{:03}".format(counter) + path.suffix
-    return Path(os.path.join(path.parent, new_file_name))
+    def make_reversed_multidict(self, a_dict):
+        reversed_multidict = dict()
+        for key, value in a_dict.items():
+            reversed_multidict.setdefault(value, list()).append(key)
+        return reversed_multidict
 
-def make_unique_paths_into(src_paths, src_to_dest_paths):
-        counter = int(0)
-        for src_path in src_paths:
-            src_to_dest_paths[src_path] = append_counter_to_path(src_to_dest_paths[src_path], counter)
-            counter += 1
+    def append_counter_to_path(self, path, counter):
+        new_file_name = path.stem + "_" + "{:03}".format(counter) + path.suffix
+        return Path(os.path.join(path.parent, new_file_name))
 
-def make_dest_paths_unique(src_to_dest_paths):
-    dest_paths_to_list_of_src_paths = make_reversed_multidict(src_to_dest_paths)
-    list_of_src_paths_with_same_dest_paths = dest_paths_to_list_of_src_paths.values()
-    for src_paths in list_of_src_paths_with_same_dest_paths:
-        make_unique_paths_into(src_paths, src_to_dest_paths)
-    return src_to_dest_paths
+    def make_unique_paths_into(self, src_paths, src_to_dest_paths):
+            counter = int(0)
+            for src_path in src_paths:
+                src_to_dest_paths[src_path] = self.append_counter_to_path(src_to_dest_paths[src_path], counter)
+                counter += 1
 
-def iterate_over_photo_files(folder, dest_folder):
-    src_dest_map = dict()
-    for fn in os.listdir(folder):
-        try:
-            file_path = os.path.join(folder, fn)
-            if os.path.isfile(file_path):
-                src_dest_map.update(src_dest_path_from_photo_creation_date(Path(file_path), dest_folder))
-        except Exception as e:
-            print(e)
-    return make_dest_paths_unique(src_dest_map)
+    def make_dest_paths_unique(self, src_to_dest_paths):
+        dest_paths_to_list_of_src_paths = self.make_reversed_multidict(src_to_dest_paths)
+        list_of_src_paths_with_same_dest_paths = dest_paths_to_list_of_src_paths.values()
+        for src_paths in list_of_src_paths_with_same_dest_paths:
+            self.make_unique_paths_into(src_paths, src_to_dest_paths)
+        return src_to_dest_paths
